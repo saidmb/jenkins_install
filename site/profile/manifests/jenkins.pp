3257deb95dd9133318554f4a22846c31baec23ad
Mohammad Bashayir Said <saidmb@Saids-MBP.hsd1.ca.comcast.net>
2018-06-05 05:37:52 -0700

    Commit

--- site/profile/manifests/jenkins.pp
+++ site/profile/manifests/jenkins.pp
@@ -1,3 +1,3 @@
 class profile::jenkins {
-  include jenkins
+  #include jenkins
 }
--- site/role/manifests/jenkins_server.pp
+++ site/role/manifests/jenkins_server.pp
@@ -1,4 +1,4 @@
 class role::jenkins_server {
   include profile::base
-  include profile::jenkins
+  #include profile::jenkins
 }
