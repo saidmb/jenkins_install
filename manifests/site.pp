6d14071719c9865e813c4cb6a8851eaf18cb37ba
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-05 01:47:15 -0700

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,20 @@
+node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a sample readme file: 123',
+    owner   => 'root',
+  }
+}
+
+node 'master.puppet.vm' {
+  include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
+}
+
+node 'jenkinstest.puppet.vm': {
+  include role::jenkins_server
+}
