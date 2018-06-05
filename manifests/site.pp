aa2db6bfcb5b12f00a1b638a424e76ccfdd5f414
Mohammad Bashayir Said <saidmb@Saids-MBP.hsd1.ca.comcast.net>
2018-06-05 13:09:12 -0700

    Commit

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file { '/root/README':
     ensure => file,
-    content => 'This is a readme: 123',
+    content => 'This is a readme: 321',
     owner   => 'root',
   }
 }
@@ -10,7 +10,7 @@ node 'master.puppet.vm' {
   include role::master_server
   file {'/root/R_master':
     ensure => file,
-    #content => “Welcome to ${fqdn}”,
+    content => Welcome to the project: 654',
     owner => 'root',
   }
 }
@@ -19,7 +19,7 @@ node 'jenkinstest.puppet.vm' {
   include role::jenkins_server
     file {'/root/R_jenkinstest':
     ensure => file,
-    #content => “Welcome to ${fqdn}”,
+    content => 'Welcome to the project: 987',
     owner => 'root',
   }
 }
