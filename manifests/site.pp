9b4d905237ad27381a777c1926709b3faa94a3fd
Mohammad Bashayir Said <saidmb@Saids-MBP.hsd1.ca.comcast.net>
2018-06-05 18:39:46 -0700

    Commit

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,16 +13,17 @@ node 'master2.puppet.vm' {
   include role::master_server
   file {'/root/R_master':
     ensure => file,
-    content => 'Welcome to the project: 654',
+    content => 'Welcome to the project: 654\n',
     owner => 'root',
   }
 }
 
 node 'jenkinstest.puppet.vm' {
-  include role::jenkins_server
-    file {'/root/R_jenkinstest':
+  file {'/root/R_jenkinstest':
     ensure => file,
-    content => 'Welcome to the project: 987',
+    content => 'Welcome to the project: 987\n',
     owner => 'root',
   }
+  include role::jenkins_server
+
 }
