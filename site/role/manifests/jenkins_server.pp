b7ecb88eba1e978d10f070d2ec8bd2057322cc5f
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-05 19:55:07 -0700

    Update jenkins_server.pp

--- site/role/manifests/jenkins_server.pp
+++ site/role/manifests/jenkins_server.pp
@@ -1,4 +1,8 @@
 class role::jenkins_server {
+
+  notify{'@@@@@@@@@@@@@ TOP OF jenkins_server.pp': }
   include profile::base
   include profile::jenkins
+  
+  notify{'@@@@@@@@@@@@@ BOTTOM OF jenkins_server.pp': }
 }
