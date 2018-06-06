5d00346250f82fe8c65a1f4ec64f6ac84b60c8e3
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-05 19:56:19 -0700

    Update master_server.pp

--- site/role/manifests/master_server.pp
+++ site/role/manifests/master_server.pp
@@ -1,4 +1,7 @@
 class role::master_server {
+  notify{'@@@@@@@@@@@@@ TOP OF master_server.pp': }
   include profile::base
   include profile::agent_nodes
+  notify{'@@@@@@@@@@@@@ BOTTOM OF master_server.pp': }
+
 }
