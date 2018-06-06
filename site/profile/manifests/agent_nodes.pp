40f4721c1cb8bf6eb9819132c8bfd6ca41f25033
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-05 19:58:10 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,11 @@
 class profile::agent_nodes {
   notify{'@@@@@@@@@@@@@ TOP OF agent_nodes.pp': }
+  
   include dockeragent
+  
+  notify{'@@@@@@@@@@@@@ MIDDLE OF agent_nodes.pp': }
+
   dockeragent::node { 'jenkinstest.puppet.vm': }
+  
   notify{'@@@@@@@@@@@@@ BOTTOM OF agent_nodes.pp': }
 }
